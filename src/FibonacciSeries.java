fbfcaa974de60079b917a6a97be06936b2d27bf4
sahil <sahil311289@gmail.com>
2018-06-02 11:01:56 -0700

    Recursion and DP

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -3,6 +3,8 @@ public class FibonacciSeries {
         System.out.println(fibo_topDown(4));
         System.out.println(fibo_bottomUp(4));
     }
+
+    // Memoization
     private static int fibo_topDown(int n) {
         if(n == 2) {
             return 1;
@@ -13,6 +15,7 @@ public class FibonacciSeries {
         return fibo_topDown(n-1) + fibo_topDown(n-2);
     }
 
+    // Tabulation
     private static int fibo_bottomUp(int n) {
         int[] series = new int[100];
         series[1] = 1;
